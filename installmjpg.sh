36507d702ea65ed487cfd731ce7b4063f4145bbf
Aaron Becker <aaron.becker@gmail.com>
2017-10-29 20:51:06 -0700

    fixed mjpg

--- installmjpg.sh
+++ installmjpg.sh
@@ -32,13 +32,24 @@ while true; do
         * ) echo "Please answer yes or no (or just y or n).";;
     esac
 done
-echo "Step 1/2: Installing required packages...";
+echo "Step 1/5: Installing required packages...";
 sudo apt-get update;
 sudo apt-get upgrade -y;
-sudo apt-get install -y kmod-video-uvc python-openssl fswebcam mjpg-streamer;
+sudo apt-get install -y libjpeg8 python-openssl fswebcam imagemagick;
 echo "Installed packages successfully.";
-echo "Step 2/2: Creating alias 'startmjpg'...";
-echo -e "\nalias startmjpg='sudo mjpg_streamer -i \"input_uvc.so -d /dev/video0 -r 640x480 -f 25\" -o \"output_http.so -p 8080 -w /www/webcam\" &'" >> ~/.bash_aliases;
+echo "Step 2/5: Downloading mjpg tarball...";
+sudo wget http://lilnetwork.com/download/raspberrypi/mjpg-streamer.tar.gz;
+echo "Downloaded tarball successfully.";
+echo "Step 3/5: Unpacking tarball...";
+tar xvzf mjpg-streamer.tar.gz;
+echo "Unpacked tarball successfully.";
+echo "Step 4/5: Compiling MJPG-Streamer (this may take a while)...";
+cd mjpg-streamer/mjpg-streamer;
+make -j4;
+echo "Compiled mjpg-streamer successfully.";
+echo "Step 5/5: Creating alias 'startmjpg'...";
+sudo cp ~/.bash_aliases ~/.bash_aliases.old;
+echo -e "\nalias startmjpg='sudo ./mjpg_streamer -i \"./input_uvc.so -d /dev/video0 -r 640x480 -f 25\" -o \"./output_http.so -p 8080 -w /www/webcam\" &'" >> ~/.bash_aliases;
 source ~/.bash_aliases;
 echo "Created command alias 'startmjpg' successfully.";
 if [ "$startstream" = "true" ]; then
